1487e43cd175e583279d4ec8d17a7964a0e0d03a
DigitalHouses <digitalhouseskz@gmail.com>
2026-09-26 06:27:55 +0500

    test: route profile target settings separately

--- dh_climate_app/tests/test_mqtt.py
+++ dh_climate_app/tests/test_mqtt.py
@@ -142,6 +142,18 @@ class MqttRoutingTests(unittest.TestCase):
         self.assertEqual("living_room", command.room_id)
         self.assertEqual("target_temperature", command.command)
 
+    def test_room_profile_target_command(self) -> None:
+        self.facade._on_message(
+            "DigitalHouses/Global/dh_climate_app/rooms/living_room/targets/set/heat_night",
+            "20.0",
+            False,
+        )
+        command = self.queue.get_nowait()
+        self.assertEqual("target", command.scope)
+        self.assertEqual("living_room", command.room_id)
+        self.assertEqual("heat_night", command.command)
+        self.assertEqual("20.0", command.payload)
+
     def test_retained_command_is_ignored(self) -> None:
         self.facade._on_message(
             "DigitalHouses/Global/dh_climate_app/season/set/target_temp_low",
